d311d0424ac98c9ef78a282afea8d2247b3575ae
David73-Coloa <davidhshs004@gmail.com>
2022-06-14 16:08:25 -0300

    Commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 const input = document.querySelector(".display")
 document.addEventListener("click",e => {
-    const target = e.target
-    input.addEventListener('keyup', e => { if (e.keyCode == 13) Calc() })
-    if (target.classList.contains('btn-num')) input.value += target.innerText             if (target.classList.contains('btn-clear')) input.value = ''
-    if (target.classList.contains('btn-del')) input.value = input.value.slice(0,-1)
-    if (target.classList.contains('btn-eq')) Calc()
+   const target = e.target
+   input.addEventListener('keyup', e => { 
+	  if (e.keyCode == 13) Calc() 
+   })
+   if (target.classList.contains('btn-num')) input.value += target.innerText                
+   if (target.classList.contains('btn-clear')) input.value = ''
+	if (target.classList.contains('btn-del')) input.value = input.value.slice(0,-1)
+	if (target.classList.contains('btn-eq')) Calc()
     
 })
 function Calc() {
